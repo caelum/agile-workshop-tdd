d0a1e76d5314db1d0d365b08e8d709c1db8671e1
Mauricio Aniche <mauricioaniche@gmail.com>
2011-02-14 17:01:32 -0200

    renaming test class

--- tests/br/com/caelum/tdd/exercise1/SalaryCalculatorTests.java
+++ tests/br/com/caelum/tdd/exercise1/SalaryCalculatorTests.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-public class EmployeeTests {
+public class SalaryCalculatorTests {
 
 	private SalaryCalculator calculator;
 
